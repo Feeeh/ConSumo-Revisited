1876497ebb0b8634a39c661db85fbf394b8d4bf2
Felipe G. Brandão <feeeh@gf2.com.br>
2021-11-22 03:41:56 -0300

    Spawning (only to the left lol)
    Still need to work on stuff :sadge:

---
+++ Assets/Scripts/SpawnItem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public float respawnTime = 1f;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(ItemWave());
+    }
+
+    private void SpawnItems()
+    {
+        GameObject item = Instantiate(itemPrefab) as GameObject;
+        item.transform.position = new Vector2(screenBounds.x * 2, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator ItemWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnItems();
+        }
+    }
+}
